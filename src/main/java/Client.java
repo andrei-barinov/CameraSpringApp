710d244c00c19bde8d99f85e1d04ad8eaaede407
Sim <vov4.games@yandex.ru>
2021-11-11 20:30:59 +0200

    Spring

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,7 +2,8 @@ public class Client {
 
     public static void main(String[] args) {
 
-        /*
+        /* данный код рабочий !
+
         Camera camera = new Camera();
         CameraRoll ColorCameraRoll = new ColorCameraRoll();
         CameraRoll BlackAndWhiteCameraRoll = new BlackAndWhiteCameraRoll();
